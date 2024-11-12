020fc671935b8bab154a2afd7b70aadc3acc9bcd
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2024-11-12 12:18:56 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({ subsets: ['latin']})
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
+})
+
+export const metadata: Metadata = {
+  title: "Pricewise",
+  description: "Track product prices effortlessly and save money on your online shopping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
